jhugon/semantic-data-taking-webapp

commit c62e662dcf4fcc3853b5add0666924efd9159608

perf: submit data now done with a single DB transaction
Before, tons of reads and writes to the database were performed by the
sparqlwrapper backend. Now, an in-memory graph is constructed with the
new data, converted to Turtule and uploaded to the DB via the graph
store protocol.

db.py
@@ -1,4 +1,4 @@
-from rdflib import Dataset, ConjunctiveGraph, Namespace, URIRef, BNode, Literal
+from rdflib import Dataset, Graph, Namespace, URIRef, BNode, Literal
 from rdflib.namespace import RDF, RDFS, XSD, SSN, SOSA
 from rdflib.store import NO_STORE, VALID_STORE
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore, _node_to_sparql
@@ -440,11 +440,10 @@ class DBInterface:
             self.data_uri_base, "stimuli", featureName.lower(), t.upper()
         )
         stimulus = self.convertToURIRef(stimulusURI)
-        self.data_graph.add((stimulus, RDF.type, SSN.Stimulus))
-        self.data_graph.set((stimulus, RDFS.comment, Literal(comment)))
-        self.data_graph.set(
-            (stimulus, self.SDTW.hasTime, Literal(t, datatype=XSD.dateTime))
-        )
+        tmp_graph = Graph()
+        tmp_graph.add((stimulus, RDF.type, SSN.Stimulus))
+        tmp_graph.set((stimulus, RDFS.comment, Literal(comment)))
+        tmp_graph.set((stimulus, self.SDTW.hasTime, Literal(t, datatype=XSD.dateTime)))
         for prop in props:
             prop = self.convertToURIRef(prop)
             datum = None
@@ -459,13 +458,13 @@ class DBInterface:
                 self.data_uri_base, "observations", featureName, propName, t
             )
             observation = self.convertToURIRef(observationURI)
-            self.data_graph.add((stimulus, SSN.isProxyFor, prop))
-            self.data_graph.add((observation, RDF.type, SOSA.Observation))
-            self.data_graph.set((observation, SOSA.madeBySensor, sensor))
-            self.data_graph.set((observation, SSN.wasOriginatedBy, stimulus))
-            self.data_graph.set((observation, SOSA.hasFeatureOfInterest, feature))
-            self.data_graph.set((observation, SOSA.observedProperty, prop))
-            self.data_graph.set(
+            tmp_graph.add((stimulus, SSN.isProxyFor, prop))
+            tmp_graph.add((observation, RDF.type, SOSA.Observation))
+            tmp_graph.set((observation, SOSA.madeBySensor, sensor))
+            tmp_graph.set((observation, SSN.wasOriginatedBy, stimulus))
+            tmp_graph.set((observation, SOSA.hasFeatureOfInterest, feature))
+            tmp_graph.set((observation, SOSA.observedProperty, prop))
+            tmp_graph.set(
                 (observation, SOSA.resultTime, Literal(t, datatype=XSD.dateTime))
             )
             proptype = self.getPropertyObservableType(prop)
@@ -479,16 +478,20 @@ class DBInterface:
                         raise DataValidationError(e)
                     unit = self.data_graph.value(prop, self.SDTW.hasUnit)
                     res = BNode()
-                    self.data_graph.set((observation, SOSA.hasResult, res))
-                    self.data_graph.set((res, RDF.type, SOSA.Result))
-                    self.data_graph.set((res, RDF.type, QUDT.Quantity))
-                    self.data_graph.set((res, QUDT.value, Literal(datum)))
-                    self.data_graph.set((res, QUDT.unit, unit))
+                    tmp_graph.set((observation, SOSA.hasResult, res))
+                    tmp_graph.set((res, RDF.type, SOSA.Result))
+                    tmp_graph.set((res, RDF.type, QUDT.Quantity))
+                    tmp_graph.set((res, QUDT.value, Literal(datum)))
+                    tmp_graph.set((res, QUDT.unit, unit))
                 case self.SDTW.categorical:
-                    self.data_graph.set((observation, SOSA.hasResult, Literal(datum)))
+                    tmp_graph.set((observation, SOSA.hasResult, Literal(datum)))
                 case other:
                     raise ValueError(f"property {prop} type not recognized: {other}")
-        self.data_graph.commit()
+        tmp_graph.commit()
+        ## Funny business with tmp_graph to only do one submission to DB rather than tons
+        ## Very significant performance improvement
+        tmp_graph_ttl = tmp_graph.serialize(format="ttl")
+        graph_store_post(self.store_path, tmp_graph_ttl, graph_uri=self.data_uri_base)
 
     def getCSV(self, feature):
         feature = self.convertToURIRef(feature)
@@ -525,7 +528,7 @@ class DBInterface:
                 ?observation sosa:hasResult ?category .
             }}"""  # nosec
 
-            LOGGER.info(category_query)
+            LOGGER.debug(category_query)
             qres = self.dataset.query(category_query)
             result = set()
             for row in qres:

commit acae939cbb9a62a44997070061294133a6c3c917

fix: fix DB getData bug introduced in c62e662dcf
The "URIs" for submitted blank nodes changed between using sparqlwrapper
and graph store protocol to upload added data. rdflib wasn't recognizing
the blank nodes as such, and thought they were just strings.

The fix uses spqrql queries instead of just rdflib to navigate the data
results. This fixes the issue and possibly speeds things up.

db.py
@@ -12,7 +12,7 @@ from io import StringIO
 import csv
 
 LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
+# LOGGER.setLevel(logging.DEBUG)
 
 QUDT = Namespace("http://qudt.org/schema/qudt/")
 
@@ -389,31 +389,59 @@ class DBInterface:
         ]
         data = []
         for stimulus in stimuli:
-            stim_data = []
-            observations = list(self.data_graph.subjects(SSN.wasOriginatedBy, stimulus))
-            for prop, proptype in zip(props, proptypes):
-                val = None
-                for observation in observations:
-                    if (observation, SOSA.observedProperty, prop) in self.data_graph:
-                        if val is None:
-                            match proptype:
-                                case self.SDTW.quantitative:
-                                    res = self.data_graph.value(
-                                        observation, SOSA.hasResult
-                                    )
-                                    val = self.data_graph.value(res, QUDT.value).value
-                                case self.SDTW.categorical:
-                                    val = self.data_graph.value(
-                                        observation, SOSA.hasResult
-                                    )
-                                case other:
-                                    raise ValueError(f"Unknown property type: {other}")
-                        else:
-                            print(
-                                f"Warning: {stimulus} {prop} has more than one observation!"
-                            )
-                stim_data.append(val)
-            data.append(stim_data)
+            quant_query = f"""
+            prefix sosa: <http://www.w3.org/ns/sosa/>
+            prefix ssn: <http://www.w3.org/ns/ssn/>
+            prefix qudt: <http://qudt.org/schema/qudt/>
+            select ?prop ?value
+            where {{
+                ?obs ssn:wasOriginatedBy {stimulus.n3()} .
+                ?obs sosa:observedProperty ?prop .
+                ?obs sosa:hasResult ?result .
+                ?result qudt:value ?value .
+            }}
+            """
+            quant_qres = list(self.data_graph.query(quant_query))
+            for row in quant_qres:
+                LOGGER.debug("        " + str(row))
+
+            other_query = f"""
+            prefix sosa: <http://www.w3.org/ns/sosa/>
+            prefix ssn: <http://www.w3.org/ns/ssn/>
+            prefix qudt: <http://qudt.org/schema/qudt/>
+            select ?prop ?result
+            where {{
+                ?obs ssn:wasOriginatedBy {stimulus.n3()} .
+                ?obs sosa:observedProperty ?prop .
+                ?obs sosa:hasResult ?result .
+                filter not exists {{ ?result qudt:value ?value . }}
+            }}
+            """
+            other_qres = list(self.data_graph.query(other_query))
+            for row in other_qres:
+                LOGGER.debug("        " + str(row))
+            propValueMap = dict()
+            for row in quant_qres + other_qres:
+                if row[0] in propValueMap:
+                    LOGGER.error(f"Propery has multiple values for stimulus")
+                    LOGGER.error(f"Propery {row[0]}")
+                    LOGGER.error(f"Stimulus {stimulus}")
+                    LOGGER.error(f"Values include:")
+                    LOGGER.error(propValueMap[row[0]])
+                    LOGGER.error(row[1])
+                    raise Exception(f"property has multiple values for stimulus")
+                else:
+                    propValueMap[row[0]] = row[1]
+            data_row = []
+            for prop in props:
+                try:
+                    value = propValueMap[prop]
+                    data_row.append(value)
+                    LOGGER.debug(f"  {prop}: {value}")
+                except KeyError:
+                    data_row.append(None)
+                    continue
+            data.append(data_row)
         return stim_times, data, stim_comments
 
     def enterData(self, feature, t, sensor, comment, datadict):